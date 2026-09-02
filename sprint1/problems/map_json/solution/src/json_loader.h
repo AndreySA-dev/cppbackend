975d77cab1b0fa7caf8fb7ad3410558d96127cdb
Andrey Semyonov <eraser.khv.dev@yandex.ru>
2026-09-02 10:49:12 +0000

    load ready

--- sprint1/problems/map_json/solution/src/json_loader.h
+++ sprint1/problems/map_json/solution/src/json_loader.h
@@ -1,11 +1,29 @@
 #pragma once
 
+#include <boost/json.hpp>
 #include <filesystem>
 
 #include "model.h"
 
 namespace json_loader {
 
-model::Game LoadGame(const std::filesystem::path& json_path);
+namespace json = boost::json;
 
-}  // namespace json_loader
+model::Point JSONToPoint(const json::value& jv);
+
+model::Size JSONToSize(const json::value& jv);
+
+model::Building JSONToBuilding(const json::value& jv);
+
+model::Offset JSONToOffset(const json::value& jv);
+
+model::Office JSONToOffice(const json::value& jv);
+
+model::Road JSONToRoad(const json::object& jo);
+
+model::Map JSONToMap(const json::object& jo);
+
+
+model::Game LoadGame(const std::filesystem::path &json_path);
+
+} // namespace json_loader
